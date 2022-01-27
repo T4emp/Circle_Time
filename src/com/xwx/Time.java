cff234117dcd1bf14c4de4f806a34e8cabf1cdeb
T4emp <67908454+T4emp@users.noreply.github.com>
2022-01-28 00:41:19 +0300

    Update Time.java

--- src/com/xwx/Time.java
+++ src/com/xwx/Time.java
@@ -1,76 +1,76 @@
 package com.xwx;
 
-public class Time {
+public class Time{
     private int second, minute, hour;
 
-    public Time(int second, int minute, int hour) {
-        if (second <= 60 && minute <= 60 && hour <= 24) {
+    public Time(int second, int minute, int hour){
+        if(second <= 60 && minute <= 60 && hour <= 24){
             this.second = second;
             this.minute = minute;
             this.hour = hour;
             System.out.println(Time.this);
         }
-        else {
+        else{
             System.out.println("Ошибка!");
         }
     }
 
-    public Time() {
+    public Time(){
         this.second = 0;
         this.minute = 0;
         this.hour = 0;
         System.out.println(Time.this);
     }
 
-    public int getSecond() {
+    public int getSecond(){
         return second;
     }
 
-    public void setSecond(int second) {
+    public void setSecond(int second){
         this.second = second;
     }
 
-    public int getMinute() {
+    public int getMinute(){
         return minute;
     }
 
-    public void setMinute(int minute) {
+    public void setMinute(int minute){
         this.minute = minute;
     }
 
-    public int getHour() {
+    public int getHour(){
         return hour;
     }
 
-    public void setHour(int hour) {
+    public void setHour(int hour){
         this.hour = hour;
     }
 
-    public String toString() {
+    public String toString(){
         return String.format("%02d:%02d:%02d", hour, minute, second);
     }
 
     public void setTime(int second, int minute, int hour){
-        if (second <= 60 && minute <= 60 && hour <= 24) {
+        if(second <= 60 && minute <= 60 && hour <= 24){
             this.second = second;
             this.minute = minute;
             this.hour = hour;
             System.out.println(Time.this);
         }
-        else {
+        else{
             System.out.println("Ошибка!");
         }
     }
 
     public Time nextSecond() {
-            ++second;
-        if(second >= 60){
+        ++second;
+        if (second >= 60) {
             second = 0;
             ++minute;
-            if(minute >= 60){
+            if (minute >= 60) {
                 minute = 0;
                 ++hour;
-                if(hour >= 24){
+                if (hour >= 24) {
                     hour = 0;
                 }
             }
